1849926df2a7a10e3d75edf942ce73ba721de066
kraymer <kraymer@gmail.Com>
2019-12-27 21:56:51 +0100

    Print only tracks added since last execution

--- sporadub.py
+++ sporadub.py
@@ -1,25 +1,56 @@
 import sys
+import pickle
+
 import spotipy
 import spotipy.util as util
 
+from datetime import datetime
+from dateutil import parser
+
 scope = 'user-library-read'
+SESSION_FILENAME = "_session.py"
 
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print("Usage: {} username".format(sys.argv[0],))
-    sys.exit()
+def store_infos(tz):
+    """Save pickle file"""
+    with open(SESSION_FILENAME, 'wb') as outfile:
+        pickle.dump(datetime.now(tz), outfile)
 
-token = util.prompt_for_user_token(username, scope)
+def load_infos():
+    """Load pickle file"""
+    try:
+        with open(SESSION_FILENAME, 'rb') as outfile:
+            return pickle.load(outfile)
+    except FileNotFoundError as e:
+        return
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    user = 'kraymer'
+def notify_new_tracks(tracks):
+    """Notify tracks added since last execution"""
+    last_execution = load_infos()
+    for track in tracks:
+        track_added_at = parser.parse(track["added_at"])
+        if not last_execution or  track_added_at > last_execution:
+            print("{} added by {}".format(
+                track["track"]["name"],
+                track["added_by"]["id"],
+                ))
+    if track_added_at:
+        store_infos(track_added_at.tzinfo)
 
-    last_track = sp.user_playlist_tracks(user, playlist_id="3X06jiX5M4MTat3aKJKMRL")['items'][-1]
-    print("{} added by {}".format(
-        last_track["track"]["name"],
-        last_track["added_by"]["id"],
-        ))
-else:
-    print("Can't get token for {}".format(username))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Usage: {} username".format(sys.argv[0],))
+        sys.exit()
+
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        user = 'kraymer'
+
+        tracks = sp.user_playlist_tracks(user, playlist_id="3X06jiX5M4MTat3aKJKMRL")['items']
+        notify_new_tracks(tracks)
+
+    else:
+        print("Can't get token for {}".format(username))
